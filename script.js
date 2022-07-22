acb3547c6ce9e06d62d5406c130b0946ca808be2
KJ O'BRIEN <kjokjo219@gmail.com>
2022-07-21 20:56:38 -0400

    Removes blog.html as it will be replaced with something else later. Creates script.js and basic code to open whateber file is entered into text box currently only resume.html and connect.html.

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function parseInput() {
+
+    var inp = document.getElementById("userInput").value
+    if(inp =="resume.html"){
+        window.location.href = "resume.html";
+    }
+    if(inp =="connect.html"){
+        window.location.href = "connect.html";
+    }
+  }
+ 

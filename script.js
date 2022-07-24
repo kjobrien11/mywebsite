1bce4c219b92fd7fffa78eccf5adf49c815f8ee9
KJ O'BRIEN <kjokjo219@gmail.com>
2022-07-24 14:54:04 -0400

    Adds basic querrying support. Allows the use of 'open ..' and lays out structure for future support

--- script.js
+++ script.js
@@ -2,23 +2,41 @@
 let input = document.querySelector('input');
 input.addEventListener('keyup', (e) => {
     if(e.key === 'Enter') {
-        parseInput();
+        const querry = document.getElementById("userInput").value.split(" ");
+        getCall(querry);
     }
 })
 function redClick(){
-    window.open('', '_self', ''); window.close();
+    window.open('', '_self', ''); 
+    window.close();
 }
-function parseInput() {
 
-    var inp = document.getElementById("userInput").value
+function getCall(querry){
+    if(querry[0] == "pwd"){
+
+    }
+    else if(querry[0] == "open"){
+        parseInput(querry[1]);
+    }else if(querry[0] == "ls"){
+        
+    }else{
+        alert('zsh: command not found: ' + querry[0])
+    }
+}
+function parseInput(querry) {
+
+    var inp = querry;
     if(inp =="resume.html"){
         window.location.href = "resume.html";
     }
-    if(inp =="connect.html"){
+    else if(inp =="connect.html"){
         window.location.href = "connect.html";
     }
-    if(inp =="about.html"){
+    else if(inp =="about.html"){
         window.location.href = "about.html";
+    }
+    else{
+        alert('The file /Users/kjobrien/Desktop/kjobrienweb/' + inp +' does not exist.');
     }
 }
 
